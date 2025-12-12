e54fb70f4106bde8c3f5cf9b7bb75852307fabd2
SuniL <sunilgumatimath.vs@gmail.com>
2025-12-12 20:34:14 +0530

    Remove node_modules and dist from git tracking
    - Remove node_modules/ directory from git cache
    - Remove dist/ directory from git cache
    - .gitignore already configured to prevent future tracking of these directories

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function greet(person) {
-    return ` Hello ${person} How you doing`;
-}
-const username = 'Ted';
-console.log(greet(username));
-//# sourceMappingURL=index.js.map
