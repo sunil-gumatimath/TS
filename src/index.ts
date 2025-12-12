62d3b722d790219393c066a5bb6c1169257cbed3
SuniL <sunilgumatimath.vs@gmail.com>
2025-12-12 19:58:13 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+function greet(person:string) : string {
+    return ` Hello ${person} How you doing`
+}
+
+const username : string = 'Ted'
+
+
+console.log(greet(username));
---
+++ src/typesInTS.ts
@@ -0,0 +1,12 @@
+let drink = 'coffee'
+
+let cups = Math.random() > 0.5 ? 10 : '5'  // inference means // → type **number | string**
+
+let channelName = 'sunilted' // inference `string`
+
+let coffeeFlavour : string = 'Caramel'   // annotations and inference as `string`
+ 
+coffeeFlavour = 'nutty'
+
+let orderCoffee : boolean = true  // annotations
+
